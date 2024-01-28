ceffa3de06596f5d17880ef1e59ce6f8a241d044
laugh12321 <laugh12321@vip.qq.com>
2024-01-28 14:34:53 +0800

    Fix License Header

--- python/export/yolov5/export.py
+++ python/export/yolov5/export.py
@@ -3,11 +3,11 @@
 # ==============================================================================
 # Copyright (c) 2024 laugh12321 Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the GNU General Public License v3.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.gnu.org/licenses/gpl-3.0.html
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
