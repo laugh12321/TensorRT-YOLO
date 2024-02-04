d4c5b6db48f9f55881f71a10063b92c6c82552f2
laugh12321 <laugh12321@vip.qq.com>
2024-02-04 08:55:16 +0800

    Optimize YOLOv8 ONNX export and remove TensorRT export

---
+++ python/export/yolov8/common.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+# ==============================================================================
+# Copyright (c) 2024 laugh12321 Authors. All Rights Reserved.
+#
+# Licensed under the GNU General Public License v3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# File    :   common.py
+# Version :   1.0
+# Author  :   laugh12321
+# Contact :   laugh12321@vip.qq.com
+# Date    :   2024/01/28 13:35:46
+# Desc    :   YOLOv8 NMS-fused detection model for TensorRT export.
+# ==============================================================================
+from typing import Tuple
+
+import math
+import torch
+import torch.nn as nn
+from torch import Tensor, Value
+
+from ultralytics.utils.tal import dist2bbox, make_anchors
+from ultralytics.nn.modules.block import DFL
+from ultralytics.nn.modules.conv import Conv
+
+__all__ = ['PostDetectTRTNMS']
+
+
+class Efficient_TRT_NMS(torch.autograd.Function):
+    """NMS block for YOLO-fused model for TensorRT."""
+
+    @staticmethod
+    def forward(
+        ctx,
+        boxes,
+        scores,
+        half: bool = False,
+        iou_threshold: float = 0.65,
+        score_threshold: float = 0.25,
+        max_output_boxes: float = 100,
+        box_coding: int = 1,
+    ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+        batch_size, num_boxes, num_classes = scores.shape
+        num_dets = torch.randint(0, max_output_boxes, (batch_size, 1), dtype=torch.int32)
+        boxes = torch.randn(batch_size, max_output_boxes, 4, dtype=torch.float16 if half else torch.float32)
+        scores = torch.randn(batch_size, max_output_boxes, dtype=torch.float16 if half else torch.float32)
+        labels = torch.randint(0, num_classes, (batch_size, max_output_boxes), dtype=torch.int32)
+
+        return num_dets, boxes, scores, labels
+
+    @staticmethod
+    def symbolic(
+        g,
+        boxes,
+        scores,
+        half: bool = False,
+        iou_threshold: float = 0.65,
+        score_threshold: float = 0.25,
+        max_output_boxes: float = 100,
+        box_coding: int = 1,
+    ) -> Tuple[Value, Value, Value, Value]:
+        out = g.op(
+            'TRT::EfficientNMS_TRT',
+            boxes,
+            scores,
+            outputs=4,
+            box_coding_i=box_coding,
+            iou_threshold_f=iou_threshold,
+            score_threshold_f=score_threshold,
+            max_output_boxes_i=max_output_boxes,
+        )
+        nums_dets, boxes, scores, classes = out
+        return nums_dets, boxes, scores, classes
+
+
+class Detect(nn.Module):
+    """YOLOv8 Detect head for detection models."""
+
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+    shape = None
+    half = False
+    max_det = 100
+    iou_thres = 0.65
+    conf_thres = 0.25
+    anchors = torch.empty(0)  # init
+    strides = torch.empty(0)  # init
+
+    def __init__(self, nc=80, ch=()):
+        """Initializes the YOLOv8 detection layer with specified number of classes and channels."""
+        super().__init__()
+        self.nc = nc  # number of classes
+        self.nl = len(ch)  # number of detection layers
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+        self.no = nc + self.reg_max * 4  # number of outputs per anchor
+        self.stride = torch.zeros(self.nl)  # strides computed during build
+        c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
+        self.cv2 = nn.ModuleList(
+            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
+        )
+        self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+
+    def forward(self, x):
+        """Concatenates and returns predicted bounding boxes and class probabilities."""
+        for i in range(self.nl):
+            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+        if self.training:  # Training path
+            return x
+
+        # Inference path
+        shape = x[0].shape  # BCHW
+        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
+        if self.dynamic or self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
+
+        if self.export and self.format in ("saved_model", "pb", "tflite", "edgetpu", "tfjs"):  # avoid TF FlexSplitV ops
+            box = x_cat[:, : self.reg_max * 4]
+            cls = x_cat[:, self.reg_max * 4 :]
+        else:
+            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
+        dbox = self.decode_bboxes(box)
+
+        if self.export and self.format in ("tflite", "edgetpu"):
+            # Precompute normalization factor to increase numerical stability
+            # See https://github.com/ultralytics/ultralytics/issues/7371
+            img_h = shape[2]
+            img_w = shape[3]
+            img_size = torch.tensor([img_w, img_h, img_w, img_h], device=box.device).reshape(1, 4, 1)
+            norm = self.strides / (self.stride[0] * img_size)
+            dbox = dist2bbox(self.dfl(box) * norm, self.anchors.unsqueeze(0) * norm[:, :2], xywh=True, dim=1)
+
+        if self.export:
+            return Efficient_TRT_NMS.apply(
+                torch.transpose(dbox, 1, 2), 
+                torch.transpose(cls.sigmoid(), 1, 2),
+                self.half,
+                self.iou_thres,
+                self.conf_thres,
+                self.max_det,
+            )
+        y = torch.cat((dbox, cls.sigmoid()), 1)
+        return (y, x)
+
+    def bias_init(self):
+        """Initialize Detect() biases, WARNING: requires stride availability."""
+        m = self  # self.model[-1]  # Detect() module
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+        # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+        for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+            a[-1].bias.data[:] = 1.0  # box
+            b[-1].bias.data[: m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+
+    def decode_bboxes(self, bboxes):
+        """Decode bounding boxes."""
+        return dist2bbox(self.dfl(bboxes), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
--- python/export/yolov8/export.py
+++ python/export/yolov8/export.py
@@ -15,15 +15,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# File    :   yolov8.py
-# Version :   1.0
+# File    :   export.py
+# Version :   2.0
 # Author  :   laugh12321
 # Contact :   laugh12321@vip.qq.com
 # Date    :   2024/01/27 15:21:07
-# Desc    :   This script exports a YOLOv8 model to TensorRT engine.
+# Desc    :   This script exports a YOLOv8 model to ONNX.
 # ==============================================================================
 import os
 import sys
+import argparse
 from pathlib import Path
 
 FILE = Path(__file__).resolve()
@@ -32,31 +33,21 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-import json
 import time
 import warnings
-import argparse
 from copy import deepcopy
-from datetime import datetime
 
 import torch
-from ultralytics.nn.autobackend import check_class_names, default_class_names
-from ultralytics.nn.modules import C2f, Detect, RTDETRDecoder
-from ultralytics.nn.tasks import SegmentationModel
-from ultralytics.utils import (
-    LINUX,
-    LOGGER,
-    __version__,
-    callbacks,
-    colorstr,
-)
-from ultralytics.utils.checks import check_imgsz, check_requirements, check_version
-from ultralytics.utils.torch_utils import get_latest_opset, smart_inference_mode
-from ultralytics.engine.exporter import try_export
-from ultralytics.utils.files import file_size
-from ultralytics import YOLO
+from common import Detect as DetectNMS
 
-from nms import PostDetectTRTNMS
+from ultralytics import YOLO
+from ultralytics.utils.files import file_size
+from ultralytics.nn.tasks import SegmentationModel
+from ultralytics.engine.exporter import try_export
+from ultralytics.utils import LOGGER, colorstr, __version__
+from ultralytics.nn.modules import C2f, Detect, RTDETRDecoder
+from ultralytics.utils.checks import check_imgsz, check_requirements
+from ultralytics.utils.torch_utils import get_latest_opset, smart_inference_mode
 
 
 class Exporter:
@@ -67,41 +58,33 @@ class Exporter:
 
     Attributes:
         args (argparse.Namespace): Configuration for the exporter.
-        callbacks (list, optional): List of callback functions. Defaults to None.
     """
 
-    def __init__(self, args: argparse.Namespace, _callbacks=None):
+    def __init__(self, args: argparse.Namespace) -> None:
         """
         Initializes the Exporter class.
 
         Args:
             args (argparse.Namespace): Configuration for the exporter.
-            _callbacks (dict, optional): Dictionary of callback functions. Defaults to None.
         """
         self.args = args
-        self.callbacks = _callbacks or callbacks.get_default_callbacks()
-        callbacks.add_integration_callbacks(self)
 
     @smart_inference_mode()
-    def __call__(self, model=None):
-        """Returns list of exported files/dirs after running callbacks."""
-        self.run_callbacks("on_export_start")
+    def __call__(self, model=None) -> None:
+        """Returns exported file."""
         t = time.time()
 
         # Device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Checks
-        if not hasattr(model, "names"):
-            model.names = default_class_names()
-        model.names = check_class_names(model.names)
-        if self.args.precision == 'fp16' and self.device.type == "cpu":
+        if self.args.half and self.device.type == "cpu":
             LOGGER.warning("WARNING ⚠️ FP16 only compatible with GPU export, i.e. use device=0")
-            self.args.precision == 'fp32'
+            self.args.half = False
         self.imgsz = check_imgsz(self.args.imgsz, stride=model.stride, min_dim=2)  # check image size
 
         # Input
-        im = torch.zeros(self.args.batch, 3, *self.imgsz).to(self.device)
+        im = torch.zeros(self.args.batch, 3, *self.imgsz, dtype=torch.float).to(self.device)
 
         # Update model
         model = deepcopy(model).to(self.device)
@@ -117,18 +100,20 @@ class Exporter:
                 m.format = 'onnx'
                 # TODO support RTDETRDecoder with nms=True
                 if isinstance(m, Detect):
-                    post_detect_class = PostDetectTRTNMS
-                    post_detect_class.conf_thres = self.args.conf_thres
-                    post_detect_class.iou_thres = self.args.iou_thres
-                    post_detect_class.max_det = self.args.max_boxes
-                    setattr(m, '__class__', post_detect_class)
+                    detect = DetectNMS
+                    detect.half = self.args.half
+                    detect.conf_thres = self.args.conf_thres
+                    detect.iou_thres = self.args.iou_thres
+                    detect.max_det = self.args.max_boxes
+                    setattr(m, '__class__', detect)
             elif isinstance(m, C2f):
                 # EdgeTPU does not support FlexSplitV while split provides cleaner ONNX graph
                 m.forward = m.forward_split
 
-        y = None
         for _ in range(2):
-            y = model(im)  # dry runs
+            model(im)  # dry runs
+        if self.args.half:
+            im, model = im.half(), model.half()  # to FP16
 
         # Filter warnings
         warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)  # suppress TracerWarning
@@ -138,55 +123,26 @@ class Exporter:
         # Assign
         self.im = im
         self.model = model
-        self.output_shape = (
-            tuple(y.shape)
-            if isinstance(y, torch.Tensor)
-            else tuple(tuple(x.shape if isinstance(x, torch.Tensor) else []) for x in y)
-        )
-        self.pretty_name = Path(self.model.yaml.get("yaml_file", self.args.weights)).stem.replace("yolo", "YOLO")
-        data = model.args["data"] if hasattr(model, "args") and isinstance(model.args, dict) else ""
-        description = f'Ultralytics {self.pretty_name} model {f"trained on {data}" if data else ""}'
-        self.metadata = {
-            "description": description,
-            "author": "Ultralytics",
-            "license": "AGPL-3.0 https://ultralytics.com/license",
-            "date": datetime.now().isoformat(),
-            "version": __version__,
-            "stride": int(max(model.stride)),
-            "task": model.task,
-            "batch": self.args.batch,
-            "imgsz": self.imgsz,
-            "names": model.names,
-        }  # model metadata
-        if model.task == "pose":
-            self.metadata["kpt_shape"] = model.model[-1].kpt_shape
-
-        LOGGER.info(
-            f"\n{colorstr('PyTorch:')} starting from '{self.args.weights}' with input shape {tuple(im.shape)} BCHW and "
-            f'output shape(s) {self.output_shape} ({file_size(self.args.weights):.1f} MB)'
-        )
+        LOGGER.info(f"\n{colorstr('PyTorch:')} starting from {self.args.weights} ({file_size(self.args.weights):.1f} MB)")
 
         # Exports
-        f, _ = self.export_engine() # TensorRT required before ONNX
+        f, _ = self.export_onnx()
 
         square = self.imgsz[0] == self.imgsz[1]
         s = (
             ""
             if square
-            else f"WARNING ⚠️ non-PyTorch val requires square images, 'imgsz={self.imgsz}' will not "
-            f"work. Use export 'imgsz={max(self.imgsz)}' if val is required."
+            else f"WARNING ⚠️ non-PyTorch val requires square images, 'imgsz={self.imgsz}' will not work."
         )
         imgsz = self.imgsz[0] if square else str(self.imgsz)[1:-1].replace(" ", "")
         LOGGER.info(
             f'\nExport complete ({time.time() - t:.1f}s)'
             f"\nResults saved to {colorstr('bold', self.args.output)}"
-            f'\nPredict:         yolo predict task={model.task} model={f} imgsz={imgsz}'
-            f'\nValidate:        yolo val task={model.task} model={f} imgsz={imgsz} data={data} {s}'
+            f'\nDetails:         yolo model={f} imgsz={imgsz} {s}'
             f'\nVisualize:       https://netron.app'
         )
 
-        self.run_callbacks("on_export_end")
-        return f  # return list of exported files/dirs
+        return f  # return exported file
 
     @try_export
     def export_onnx(self, prefix=colorstr("ONNX:")):
@@ -199,7 +155,7 @@ class Exporter:
 
         opset_version = self.args.opset or get_latest_opset()
         LOGGER.info(f"\n{prefix} starting export with onnx {onnx.__version__} opset {opset_version}...")
-        # f = str(self.file.with_suffix(".onnx"))
+
         f = str(Path(self.args.output, Path(self.args.weights).stem).with_suffix(".onnx"))
 
         output_names = ["output0", "output1"] if isinstance(self.model, SegmentationModel) else ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes']
@@ -242,100 +198,21 @@ class Exporter:
             except Exception as e:
                 LOGGER.info(f"{prefix} simplifier failure: {e}")
 
-        # Metadata
-        for k, v in self.metadata.items():
-            meta = model_onnx.metadata_props.add()
-            meta.key, meta.value = k, str(v)
-
         onnx.save(model_onnx, f)
         return f, model_onnx
 
 
-    @try_export
-    def export_engine(self, prefix=colorstr("TensorRT:")):
-        """YOLOv8 TensorRT export https://developer.nvidia.com/tensorrt."""
-        assert self.im.device.type != "cpu", "export running on CPU but must be on GPU, i.e. use 'device=0'"
-        f_onnx, _ = self.export_onnx()  # run before trt import https://github.com/ultralytics/ultralytics/issues/7016
-
-        try:
-            import tensorrt as trt  # noqa
-        except ImportError:
-            if LINUX:
-                check_requirements("nvidia-tensorrt", cmds="-U --index-url https://pypi.ngc.nvidia.com")
-            import tensorrt as trt  # noqa
-
-        check_version(trt.__version__, "7.0.0", hard=True)  # require tensorrt>=7.0.0
-
-        self.args.simplify = True
-
-        LOGGER.info(f"\n{prefix} starting export with TensorRT {trt.__version__}...")
-        assert Path(f_onnx).exists(), f"failed to export ONNX file: {f_onnx}"
-        # f = self.file.with_suffix(".engine")  # TensorRT engine file
-        f = str(Path(self.args.output, Path(self.args.weights).stem).with_suffix(".engine"))
-        logger = trt.Logger(trt.Logger.INFO)
-        if self.args.verbose:
-            logger.min_severity = trt.Logger.Severity.VERBOSE
-
-        trt.init_libnvinfer_plugins(logger, namespace="")
-
-        builder = trt.Builder(logger)
-        config = builder.create_builder_config()
-        config.max_workspace_size = self.args.workspace * 1 << 30
-        # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace << 30)  # fix TRT 8.4 deprecation notice
-
-        flag = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-        network = builder.create_network(flag)
-        parser = trt.OnnxParser(network, logger)
-        if not parser.parse_from_file(f_onnx):
-            raise RuntimeError(f"failed to load ONNX file: {f_onnx}")
-
-        inputs = [network.get_input(i) for i in range(network.num_inputs)]
-        outputs = [network.get_output(i) for i in range(network.num_outputs)]
-        for inp in inputs:
-            LOGGER.info(f'{prefix} input "{inp.name}" with shape{inp.shape} {inp.dtype}')
-        for out in outputs:
-            LOGGER.info(f'{prefix} output "{out.name}" with shape{out.shape} {out.dtype}')
-
-        LOGGER.info(
-            f"{prefix} building FP{16 if builder.platform_has_fast_fp16 and self.args.precision == 'fp16' else 32} engine as {f}"
-        )
-        if builder.platform_has_fast_fp16 and self.args.precision == 'fp16':
-            config.set_flag(trt.BuilderFlag.FP16)
-            config.set_flag(trt.BuilderFlag.STRICT_TYPES)
-
-        del self.model
-        torch.cuda.empty_cache()
-
-        # Write file
-        with builder.build_serialized_network(network, config) as engine, open(f, "wb") as t:
-            t.write(engine)
-            LOGGER.info(f"Serialize engine success, saved as {f}")
-
-        return f, None
-
-    def add_callback(self, event: str, callback):
-        """Appends the given callback."""
-        self.callbacks[event].append(callback)
-
-    def run_callbacks(self, event: str):
-        """Execute all callbacks for a given event."""
-        for callback in self.callbacks.get(event, []):
-            callback(self)
-
-
 def parse_opt() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Export YOLOv8 model to TensorRT engine.')
-    parser.add_argument('-w', '--weights', required=True, help='Path to Ultralytics YOLOv8 model weights file.')
+    parser = argparse.ArgumentParser(description='Export YOLOv5 model to ONNX.')
+    parser.add_argument('-w', '--weights', required=True, help='Path to Ultralytics YOLOv5 model weights file.')
     parser.add_argument('-o', '--output', required=True, type=str, help='Directory path to save the exported model.')
     parser.add_argument('-b', '--batch', type=int, default=1, help='Total batch size for the model.')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='Inference size (height, width).')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='Confidence threshold for object detection.')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold for post-processing.')
     parser.add_argument('--max-boxes', type=int, default=100, help='Maximum number of detections to output per image.')
-    parser.add_argument('-p', '--precision', type=str, default='fp32', choices=['fp32', 'fp16'], help="Data type for the engine inference.")
     parser.add_argument('-s', '--simplify', action='store_true', help='Whether to simplify the exported ONNX. Default is False.')
-    parser.add_argument('--verbose', action='store_true', help='Enable verbose output.')
-    parser.add_argument('--workspace', type=int, default=4, help='Workspace size for TensorRT engine.')
+    parser.add_argument("--half", action="store_true", help="FP16 half-precision export")
     parser.add_argument('--opset', type=int, default=11, help='ONNX opset version.')
 
     opt = parser.parse_args()
@@ -350,5 +227,5 @@ if __name__ == '__main__':
     # Load a model
     model = YOLO(opt.weights)
 
-    # Export TensorRT engine
+    # Export ONNX model
     Exporter(args=opt)(model=model.model)
