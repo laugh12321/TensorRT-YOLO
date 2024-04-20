81ab751ddefcee800741dcd26282920e21f645ab
laugh12321 <laugh12321@vip.qq.com>
2024-04-20 19:49:17 +0800

    Refactor C++ detect demo

--- demo/detect/detect.cpp
+++ demo/detect/detect.cpp
@@ -1,33 +1,33 @@
-#include "include/common.hpp"
-#include "include/cuda_utils.hpp"
-#include "include/detection.hpp"
-
 #include <CLI/CLI.hpp>
-#include <iostream>
+#include <chrono>
 #include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include "deploy/utils/utils.hpp"
+#include "deploy/vision/detection.hpp"
+#include "deploy/vision/visualize.hpp"
 
 namespace fs = std::filesystem;
 
-std::vector<std::string> getImagesInDirectory(const std::string& folderPath) {
-    std::vector<std::string> imageFiles;
-
-    for (const auto& entry : fs::directory_iterator(folderPath)) {
+std::vector<std::string> GetImagesInDirectory(const std::string& folder_path) {
+    std::vector<std::string> image_files;
+    for (const auto& entry : fs::directory_iterator(folder_path)) {
         if (fs::is_regular_file(entry)) {
             const auto extension = entry.path().extension().string();
             if (extension == ".jpg" || extension == ".png" || extension == ".jpeg" || extension == ".bmp") {
-                imageFiles.push_back(entry.path().string());
+                image_files.push_back(entry.path().string());
             }
         }
     }
-
-    return imageFiles;
+    return image_files;
 }
 
-std::string getFileName(const std::string& filePath) {
-    return fs::path(filePath).filename().string();
+std::string GetFileName(const std::string& file_path) {
+    return fs::path(file_path).filename().string();
 }
 
-void createOutputDirectory(const std::string& output) {
+void CreateOutputDirectory(const std::string& output) {
     if (!fs::exists(output)) {
         if (!fs::create_directories(output)) {
             std::cerr << "Failed to create output directory: " << output << std::endl;
@@ -41,69 +41,83 @@ void createOutputDirectory(const std::string& output) {
     }
 }
 
+void ProcessSingleImage(const std::string& image_path, const std::shared_ptr<deploy::DeployDet>& model, const std::vector<std::pair<std::string, cv::Scalar>>& labels, const std::string& output_path) {
+    cv::Mat image  = cv::imread(image_path);
+    auto    result = model->Predict(image);
+    if (!output_path.empty()) {
+        deploy::Visualize(image, result, labels);
+        cv::imwrite(output_path + "/" + GetFileName(image_path), image);
+    }
+}
+
+void ProcessBatchImages(const std::vector<std::string>& image_files, const std::shared_ptr<deploy::DeployDet>& model, const std::vector<std::pair<std::string, cv::Scalar>>& labels, const std::string& output_path) {
+    const size_t                                         batch_size = model->batch_size;
+    deploy::GpuTimer                                     gpu_timer;
+    deploy::CpuTimer<std::chrono::high_resolution_clock> cpu_timer;
+    for (size_t i = 0; i < image_files.size(); i += batch_size) {
+        std::vector<cv::Mat>     img_batch;
+        std::vector<std::string> img_name_batch;
+        for (size_t j = i; j < i + batch_size && j < image_files.size(); ++j) {
+            img_name_batch.push_back(GetFileName(image_files[j]));
+            img_batch.push_back(cv::imread(image_files[j]));
+        }
+        cpu_timer.Start();
+        gpu_timer.Start();
+        auto results = model->Predict(img_batch);
+        gpu_timer.Stop();
+        cpu_timer.Stop();
+        std::cout << img_batch.size() << " Batch infer CPU elapsed time: " << cpu_timer.Microseconds() / 1000 << " ms" << std::endl;
+        std::cout << img_batch.size() << " Batch infer GPU elapsed time: " << gpu_timer.Microseconds() / 1000 << " ms" << std::endl;
+        gpu_timer.Reset();
+        cpu_timer.Reset();
+        if (!output_path.empty()) {
+            for (size_t j = 0; j < img_batch.size(); ++j) {
+                deploy::Visualize(img_batch[j], results[j], labels);
+                cv::imwrite(output_path + "/" + img_name_batch[j], img_batch[j]);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     CLI::App app{"YOLO Series Inference Script."};
 
-    std::string enginePath, inputPath, outputPath, labelPath;
-    app.add_option("-e, --engine", enginePath, "Serialized TensorRT engine.")->required()->check(CLI::ExistingFile);
-    app.add_option("-i, --input", inputPath, "Path to image or directory.")->required()->check(CLI::ExistingPath);
-    app.add_option("-o, --output", outputPath, "Directory to save results.");
-	app.add_option("-l, --labels", labelPath, "File to use for reading the class labels from")->check(CLI::ExistingFile);
+    std::string engine_path;
+    std::string input_path;
+    std::string output_path;
+    std::string label_path;
+    app.add_option("-e, --engine", engine_path, "Serialized TensorRT engine.")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("-i, --input", input_path, "Path to image or directory.")
+        ->required()
+        ->check(CLI::ExistingPath);
+    app.add_option("-o, --output", output_path, "Directory to save results.");
+    app.add_option("-l, --labels", label_path, "File to use for reading the class labels from")
+        ->check(CLI::ExistingFile);
 
     CLI11_PARSE(app, argc, argv);
 
-    auto engine = yolo::load(enginePath);
-    if (!engine) throw std::runtime_error("Failed to construct engine context.");
-    std::cout << "Engine context constructed successfully." << std::endl;
+    auto model = std::make_shared<deploy::DeployDet>(engine_path);
 
-    std::vector<std::pair<std::string, cv::Scalar>> labelColors;
-    if (!outputPath.empty()) {
-        labelColors = generateLabelsWithColors(labelPath);
-        createOutputDirectory(outputPath);
+    std::vector<std::pair<std::string, cv::Scalar>> labels;
+    if (!output_path.empty()) {
+        labels = deploy::GenerateLabelColorParis(label_path);
+        CreateOutputDirectory(output_path);
     }
 
-    if (fs::is_regular_file(inputPath)) {
-        cv::Mat image = cv::imread(inputPath);
-        auto detectInfo = engine->predict(image);
-
-        if (!outputPath.empty()) {
-            visualize(image, detectInfo, labelColors);
-            cv::imwrite(outputPath + "/" + getFileName(inputPath), image);
-        }
+    if (fs::is_regular_file(input_path)) {
+        ProcessSingleImage(input_path, model, labels, output_path);
     } else {
-        cuda_utils::GpuTimer gpuTimer;
-        cuda_utils::CpuTimer<std::chrono::high_resolution_clock> cpuTimer;
-
-        auto imageFiles = getImagesInDirectory(inputPath);
-        const size_t batchSize = engine->batchSize;
-        for (size_t i = 0; i < imageFiles.size(); i += batchSize) {
-            std::vector<cv::Mat> imgBatch;
-            std::vector<std::string> imgNameBatch;
-            for (size_t j = i; j < i + batchSize && j < imageFiles.size(); ++j) {
-                imgNameBatch.push_back(getFileName(imageFiles[j]));
-                imgBatch.push_back(cv::imread(imageFiles[j]));
-            }
-
-            cpuTimer.start();
-            gpuTimer.start();
-            auto detectInfos = engine->predict(imgBatch);
-            gpuTimer.stop();
-            cpuTimer.stop();
-
-            std::cout << imgBatch.size() << " Batch infer CPU elapsed time: " << cpuTimer.milliseconds() << " ms" << std::endl;
-            std::cout << imgBatch.size() << " Batch infer GPU elapsed time: " << gpuTimer.milliseconds() << " ms" << std::endl;
-            gpuTimer.reset();
-            cpuTimer.reset();
-
-            if (!outputPath.empty()) {
-                for (size_t j = 0; j < imgBatch.size(); ++j) {
-                    visualize(imgBatch[j], detectInfos[j], labelColors);
-                    cv::imwrite(outputPath + "/" + imgNameBatch[j], imgBatch[j]);
-                }
-            }
+        auto image_files = GetImagesInDirectory(input_path);
+        if (!image_files.empty()) {
+            ProcessBatchImages(image_files, model, labels, output_path);
+        } else {
+            std::cerr << "No images found in directory: " << input_path << std::endl;
+            return 1;
         }
     }
 
-	std::cout << "Inference completed." << std::endl;
+    std::cout << "Inference completed." << std::endl;
     return 0;
 }
---
+++ demo/detect/xmake.lua
@@ -0,0 +1,78 @@
+-- 添加编译规则
+add_rules("mode.debug", "mode.release")
+set_allowedplats("windows", "linux")
+add_requires("opencv", "cli11")
+if is_host("linux") then 
+    add_requires("zlib")
+end
+
+-- 定义选项
+option("tensorrt")
+    set_showmenu(true)
+    set_description("TensorRT Path. Example: /usr/local/tensorrt")
+    on_check(function (option)
+        if not option:enabled() then 
+            raise("TensorRT path is not set. Please specify the TensorRT path.")
+        end 
+    end)
+
+option("deploy")
+    set_showmenu(true)
+    set_description("TensorRT-YOLO Project Path.")
+    on_check(function (option)
+        if not option:enabled() then 
+            raise("TensorRT-YOLO project path is not set. Please specify the TensorRT-YOLO Project path.")
+        end 
+    end)
+
+-- 定义目标
+target("detect")
+    set_languages("cxx17")
+    set_rundir("$(projectdir)")
+    add_packages("opencv", "cli11")
+    if is_host("linux") then 
+        add_packages("zlib")
+    end
+
+    set_runargs("-e model/yolov8s.engine", "-i input", "-o output", "-l labels.txt")
+
+    -- 添加文件
+    add_files("detect.cpp")
+
+    -- 设置目标类型
+    set_kind("binary")
+
+    -- 添加 cuda
+    add_rules("cuda")
+    add_cugencodes("native")
+
+    -- 添加deploy链接目录和链接库
+    if has_config("deploy") then
+        add_includedirs(path.join("$(deploy)", "include"))
+        add_linkdirs(path.join("$(deploy)", "lib"))
+        add_links("deploy")
+
+        -- 复制deploy库文件到项目目录
+        after_build(function (target)
+
+            -- 定义deploy库文件路径
+            local deploy_lib_name = is_host("windows") and "deploy.dll" or "libdeploy.so"
+            local deploy_lib_path = path.join("$(deploy)", "lib", deploy_lib_name)
+
+            if os.isfile(deploy_lib_path) then
+
+                -- 目标文件路径
+                local deploy_lib_target_path = path.join(target:targetdir(), deploy_lib_name)
+            
+                -- 复制文件
+                os.cp(deploy_lib_path, deploy_lib_target_path)
+            end
+        end)
+    end
+
+    -- 添加TensorRT链接目录和链接库
+    if has_config("tensorrt") then
+        add_includedirs(path.join("$(tensorrt)", "include"))
+        add_linkdirs(path.join("$(tensorrt)", "lib"))
+        add_links("nvinfer", "nvinfer_plugin", "nvparsers", "nvonnxparser")
+    end
