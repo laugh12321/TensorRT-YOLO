54897b6e9395f261a96b4390dd6421020a8f22cf
laugh12321 <laugh12321@vip.qq.com>
2025-01-09 13:51:34 +0800

    feat: Add image preprocessing and inference option configuration structs
    - Introduce `option.hpp` file, defining `ProcessConfig` for image preprocessing and `InferOption` for inference options.
    - Provide methods for setting channel swap, border value, normalization parameters, etc.

---
+++ include/deploy/core/option.hpp
@@ -0,0 +1,129 @@
+/**
+ * @file option.hpp
+ * @author laugh12321 (laugh12321@vip.qq.com)
+ * @brief 定义图像预处理配置和推理选项配置的结构体
+ * @date 2025-01-09
+ *
+ * @copyright Copyright (c) 2025 laugh12321. All Rights Reserved.
+ *
+ */
+
+#pragma once
+
+#include <cassert>
+#include <vector>
+#include <vector_functions.hpp>
+
+namespace deploy {
+
+/**
+ * @brief 图像预处理配置结构体
+ *
+ */
+struct ProcessConfig {
+    bool   swap_rb      = false;                                                  // < 是否交换 R 和 B 通道
+    float  border_value = 114.0f;                                                 // < 边界值，用于填充
+    float3 alpha        = make_float3(1.0 / 255.0f, 1.0 / 255.0f, 1.0 / 255.0f);  // < 归一化系数
+    float3 beta         = make_float3(0.0f, 0.0f, 0.0f);                          // < 偏移量
+
+    /**
+     * @brief 设置图像通道交换
+     *
+     * @param swap_rb
+     */
+    void setSwapRb(bool swap_rb) {
+        this->swap_rb = swap_rb;
+    }
+
+    /**
+     * @brief 设置边界值
+     *
+     * @param border_value
+     */
+    void setBorderValue(float border_value) {
+        this->border_value = border_value;
+    }
+
+    /**
+     * @brief 设置归一化参数
+     *
+     * @param mean
+     * @param std
+     */
+    void setNormalizeParams(const std::vector<float>& mean, const std::vector<float>& std) {
+        assert(mean.size() == 3 && std.size() == 3 && "ProcessConfig: requires the size of mean and std to be 3.");
+
+        alpha.x = 1.0 / 255.0f / std[0];
+        alpha.y = 1.0 / 255.0f / std[1];
+        alpha.z = 1.0 / 255.0f / std[2];
+        beta.x  = -mean[0] / std[0];
+        beta.y  = -mean[1] / std[1];
+        beta.z  = -mean[2] / std[2];
+    }
+};
+
+/**
+ * @brief 推理选项配置结构体
+ *
+ */
+struct InferOption {
+    int           device_id             = 0;      // < GPU ID
+    bool          cuda_mem              = false;  // < 推理数据是否已经在 CUDA 显存中
+    bool          enable_managed_memory = false;  // < 是否启用统一内存
+    ProcessConfig config;                         // < 图像预处理配置
+
+    /**
+     * @brief 设置 GPU 设备 ID
+     *
+     * @param id
+     */
+    void setDeviceId(int id) {
+        this->device_id = id;
+    }
+
+    /**
+     * @brief 设置推理数据在 CUDA 显存中
+     *
+     */
+    void enableCudaMem() {
+        this->cuda_mem = true;
+    }
+
+    /**
+     * @brief 是否启用统一内存
+     *
+     */
+    void enableManagedMemory() {
+        this->enable_managed_memory = true;
+    }
+
+    /**
+     * @brief 设置图像通道交换
+     *
+     * @param swap_rb
+     */
+    void setSwapRb(bool swap_rb) {
+        config.setSwapRb(swap_rb);
+    }
+
+    /**
+     * @brief 设置边界值
+     *
+     * @param border_value
+     */
+    void setBorderValue(float border_value) {
+        config.setBorderValue(border_value);
+    }
+
+    /**
+     * @brief 设置归一化参数
+     *
+     * @param mean
+     * @param std
+     */
+    void setNormalizeParams(const std::vector<float>& mean, const std::vector<float>& std) {
+        config.setNormalizeParams(mean, std);
+    }
+};
+
+}  // namespace deploy
