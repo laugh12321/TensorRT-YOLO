e672ac9350d122e43453628546957f0b30b578dc
laugh12321 <laugh12321@vip.qq.com>
2025-01-18 17:00:54 +0800

    feat: Add << overload methods for data structures

--- include/deploy/infer/result.hpp
+++ include/deploy/infer/result.hpp
@@ -39,6 +39,11 @@ struct DEPLOYAPI Image {
             throw std::invalid_argument(MAKE_ERROR_MESSAGE("Image: width and height must be positive"));
         }
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const Image& img) {
+        os << "Image(width=" << img.width << ", height=" << img.height << ", ptr=" << img.ptr << ")";
+        return os;
+    }
 };
 
 /**
@@ -66,6 +71,11 @@ struct DEPLOYAPI Mask {
             throw std::invalid_argument(MAKE_ERROR_MESSAGE("Mask: width and height must be positive"));
         }
         data.resize(width * height);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Mask& mask) {
+        os << "Mask(width=" << mask.width << ", height=" << mask.height << ", data size=" << mask.data.size() << ")";
+        return os;
     }
 
     Mask(const Mask& other)            = default;      // < 默认拷贝构造函数
@@ -98,6 +108,15 @@ struct DEPLOYAPI KeyPoint {
      */
     KeyPoint(float x, float y, std::optional<float> conf = std::nullopt)
         : x(x), y(y), conf(conf) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const KeyPoint& kp) {
+        os << "KeyPoint(x=" << kp.x << ", y=" << kp.y;
+        if (kp.conf) {
+            os << ", conf=" << *kp.conf;
+        }
+        os << ")";
+        return os;
+    }
 
     KeyPoint(const KeyPoint& other)            = default;      // < 默认拷贝构造函数
     KeyPoint& operator=(const KeyPoint& other) = default;      // < 默认拷贝赋值运算符
@@ -132,6 +151,11 @@ struct DEPLOYAPI Box {
     Box(float left, float top, float right, float bottom)
         : left(left), top(top), right(right), bottom(bottom) {}
 
+    friend std::ostream& operator<<(std::ostream& os, const Box& box) {
+        os << "Box(left=" << box.left << ", top=" << box.top << ", right=" << box.right << ", bottom=" << box.bottom << ")";
+        return os;
+    }
+
     Box(const Box& other)            = default;      // < 默认拷贝构造函数
     Box& operator=(const Box& other) = default;      // < 默认拷贝赋值运算符
 
@@ -162,6 +186,11 @@ struct DEPLOYAPI RotatedBox : public Box {
      */
     RotatedBox(float left, float top, float right, float bottom, float theta)
         : Box(left, top, right, bottom), theta(theta) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const RotatedBox& rbox) {
+        os << "RotatedBox(left=" << rbox.left << ", top=" << rbox.top << ", right=" << rbox.right << ", bottom=" << rbox.bottom << ", theta=" << rbox.theta << ")";
+        return os;
+    }
 
     RotatedBox(const RotatedBox& other)            = default;      // < 默认拷贝构造函数
     RotatedBox& operator=(const RotatedBox& other) = default;      // < 默认拷贝赋值运算符
@@ -206,6 +235,15 @@ struct DEPLOYAPI BaseRes {
  */
 struct DEPLOYAPI ClassifyRes : public BaseRes {
     // 无需额外的构造函数，继承自 BaseRes 的默认构造函数已经足够
+
+    friend std::ostream& operator<<(std::ostream& os, const BaseRes& res) {
+        os << "ClassifyRes(\n    num=" << res.num << ",\n    classes=[";
+        for (const auto& c : res.classes) os << c << ", ";
+        os << "],\n    scores=[";
+        for (const auto& s : res.scores) os << s << ", ";
+        os << "]\n)";
+        return os;
+    }
 };
 
 /**
@@ -230,6 +268,17 @@ struct DEPLOYAPI DetectRes : public BaseRes {
      */
     DetectRes(int num, const std::vector<int>& classes, const std::vector<float>& scores, const std::vector<Box>& boxes)
         : BaseRes(num, classes, scores), boxes(boxes) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const DetectRes& res) {
+        os << "DetectRes(\n    num=" << res.num << ",\n    classes=[";
+        for (const auto& c : res.classes) os << c << ", ";
+        os << "],\n    scores=[";
+        for (const auto& s : res.scores) os << s << ", ";
+        os << "],\n    boxes=[\n";
+        for (const auto& box : res.boxes) os << "        " << box << ",\n";
+        os << "    ]\n)";
+        return os;
+    }
 
     DetectRes(const DetectRes& other)            = default;      // < 默认拷贝构造函数
     DetectRes& operator=(const DetectRes& other) = default;      // < 默认拷贝赋值运算符
@@ -260,6 +309,17 @@ struct DEPLOYAPI OBBRes : public BaseRes {
      */
     OBBRes(int num, const std::vector<int>& classes, const std::vector<float>& scores, const std::vector<RotatedBox>& boxes)
         : BaseRes(num, classes, scores), boxes(boxes) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const OBBRes& res) {
+        os << "OBBRes(\n    num=" << res.num << ",\n    classes=[";
+        for (const auto& c : res.classes) os << c << ", ";
+        os << "],\n    scores=[";
+        for (const auto& s : res.scores) os << s << ", ";
+        os << "],\n    boxes=[\n";
+        for (const auto& box : res.boxes) os << "        " << box << ",\n";
+        os << "    ]\n)";
+        return os;
+    }
 
     OBBRes(const OBBRes& other)            = default;      // < 默认拷贝构造函数
     OBBRes& operator=(const OBBRes& other) = default;      // < 默认拷贝赋值运算符
@@ -293,6 +353,19 @@ struct DEPLOYAPI SegmentRes : public BaseRes {
     SegmentRes(int num, const std::vector<int>& classes, const std::vector<float>& scores, const std::vector<Box>& boxes, const std::vector<Mask>& masks)
         : BaseRes(num, classes, scores), boxes(boxes), masks(masks) {}
 
+    friend std::ostream& operator<<(std::ostream& os, const SegmentRes& res) {
+        os << "SegmentRes(\n    num=" << res.num << ",\n    classes=[";
+        for (const auto& c : res.classes) os << c << ", ";
+        os << "],\n    scores=[";
+        for (const auto& s : res.scores) os << s << ", ";
+        os << "],\n    boxes: [\n";
+        for (const auto& box : res.boxes) os << "        " << box << ",\n";
+        os << "],\n    masks: [\n";
+        for (const auto& mask : res.masks) os << "        " << mask << "\n";
+        os << "    ]\n)";
+        return os;
+    }
+
     SegmentRes(const SegmentRes& other)            = default;      // < 默认拷贝构造函数
     SegmentRes& operator=(const SegmentRes& other) = default;      // < 默认拷贝赋值运算符
 
@@ -324,6 +397,23 @@ struct DEPLOYAPI PoseRes : public BaseRes {
      */
     PoseRes(int num, const std::vector<int>& classes, const std::vector<float>& scores, const std::vector<Box>& boxes, const std::vector<std::vector<KeyPoint>>& kpts)
         : BaseRes(num, classes, scores), boxes(boxes), kpts(kpts) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const PoseRes& res) {
+        os << "PoseRes(\n    num=" << res.num << ",\n    classes=[";
+        for (const auto& c : res.classes) os << c << ", ";
+        os << "],\n    scores=[";
+        for (const auto& s : res.scores) os << s << ", ";
+        os << "],\n    boxes=[\n";
+        for (const auto& box : res.boxes) os << "        " << box << "\n";
+        os << "],\n    kpts=[\n";
+        for (const auto& kp_list : res.kpts) {
+            os << "        [ ";
+            for (const auto& kp : kp_list) os << "            " << kp << ", ";
+            os << "        ],\n";
+        }
+        os << "    ]\n)";
+        return os;
+    }
 
     PoseRes(const PoseRes& other)            = default;      // < 默认拷贝构造函数
     PoseRes& operator=(const PoseRes& other) = default;      // < 默认拷贝赋值运算符
